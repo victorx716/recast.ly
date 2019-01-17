ae50b7985b60ccb3b310769cf891227863fca021
victorx716 <victorliu716@gmail.com>
2019-01-16 17:33:45 -0800

    Imported reactDOM to render the app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // TODO: Render the `App` component to the DOM
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+
+ReactDOM.render(<App/>, document.getElementById('app'));

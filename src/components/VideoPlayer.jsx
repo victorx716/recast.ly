91bc34034bd180c513fe863f6cd172d745c50d0e
victorx716 <victorliu716@gmail.com>
2019-01-16 22:25:56 -0800

    Renders video in video player dynamically

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,7 +3,7 @@ import VideoList from './VideoList.js';
 var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/4ZAEBxGipoA" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + props.video.id.videoId} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
       <h3>{props.video.snippet.title}</h3>

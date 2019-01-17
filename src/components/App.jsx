f8006a6c7316950d1d9c46e6c2098e34a3c60e98
victorx716 <victorliu716@gmail.com>
2019-01-17 12:15:01 -0800

    Adds initial setting state and refactored into class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,62 @@ import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import Search from './Search.js';
 import VideoPlayer from './VideoPlayer.js';
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current: exampleVideoData[0],
+      select: false,
+      videos: exampleVideoData
+    }
+  }
+
+onVideoClick() {
+  this.setState({
+    current: 
+    });
+}
+
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.current}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
+
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <Search />
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <VideoPlayer video={exampleVideoData[0]}/>
+//       </div>
+//       <div className="col-md-5">
+//         <VideoList videos={exampleVideoData}/>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
